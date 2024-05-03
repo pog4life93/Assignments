9870efaf5e8ee946afe355544a5b0bc3bac37171
pog4life93 <pog4life93@gmail.com>
2024-05-02 21:50:35 -0700

    Assignment 6 Upload.

--- Assignment6/starter/encrypt-it.js
+++ Assignment6/starter/encrypt-it.js
@@ -1,26 +1,50 @@
-/*
- * Starter file 
- */
-(function() {
-  "use strict";
+(function () {
+  'use strict';
+  console.log('Window loaded!');
 
-  /**
-   * The starting point in our program, setting up a listener
-   * for the "load" event on the window, signalling the HTML DOM has been constructed
-   * on the page. When this event occurs, the attached function (init) will be called.
-   */
-  window.addEventListener("load", init);
 
-  /**
-   * TODO: Write a function comment using JSDoc.
-   */
+  window.addEventListener('load', init);
+  const encrypt = document.querySelector('#encrypt-it');
+
+  const text = document.querySelector('#input-text');
+
+  const result = document.querySelector('#result');
+  const reset = document.querySelector('#reset');
+
+  encrypt.addEventListener('click', encryptClick);
+  reset.addEventListener('click', resetClick);
+
   function init() {
-    // Note: In this function, we usually want to set up our event handlers
-    // for UI elements on the page.
+      console.log('loaded');
+  }
+  
+  function resetClick() {
+    result.innerHTML = '';
+    text.value = '';
   }
 
-  // Add any other functions in this area (you should not implement your
-  // entire program in the init function, for similar reasons that
-  // you shouldn't write an entire Java program in the main method).
+  function encryptClick(e) {
+      console.log('Button clicked!');
+      const newText = shiftCipher(text.value);
 
+      result.innerHTML = newText;
+  }
+
+  function shiftCipher(text) {
+      text = text.toLowerCase();
+      let result = '';
+      for (let i = 0; i < text.length; i++) {
+          if (text[i] < 'a' || text[i] > 'z') {
+              result += text[i];
+          } else if (text[i] == 'z') {
+              result += 'a';
+          } else {
+              const SHIFT = 1;
+              let letter = text.charCodeAt(i);
+              let resultLetter = String.fromCharCode(letter + SHIFT);
+              result += resultLetter;
+          }
+      }
+      return result;
+  }
 })();
